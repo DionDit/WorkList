401c1a4dc015e4ad07d7c68581dfa76998d19bd1
Денис Лебедев <denis03lebedev@gmail.com>
2020-01-26 13:23:58 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> A = new List<double>();
+            int l;
+            Console.WriteLine("Введите длину массива");
+            l = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Заполните массив");
+            for (int i = 0; i < l; i++)
+            {
+
+                A.Add(Convert.ToDouble(Console.ReadLine()));
+            }
+
+        }
+    }
+}
