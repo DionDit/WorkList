ae96db652cc0b18aee829b15c3a3ca8911c8e8f1
Денис Лебедев <denis03lebedev@gmail.com>
2020-01-26 14:10:53 +0700

    Был добавлен класс с работой листа

--- Program.cs
+++ Program.cs
@@ -11,17 +11,29 @@ namespace WorkList
         static void Main(string[] args)
         {
             List<double> A = new List<double>();
-            int l;
-            Console.WriteLine("Введите длину массива");
-            l = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-            Console.WriteLine("Заполните массив");
-            for (int i = 0; i < l; i++)
+            int l,n;
+            while (true)
             {
-
-                A.Add(Convert.ToDouble(Console.ReadLine()));
+                Console.Clear();
+                Console.WriteLine("Выберете что хотите сделать\n1)Заполнить массив\n2)Вывести лист на экран\n3)Добавить новый элемент в лист\n4)Вывести на экран макс.элемент листа");
+                n = Convert.ToInt32(Console.ReadLine());
+                switch (n)
+                {
+                    case 1:
+                        Console.Clear();
+                        WList.FillList(A);
+                        break;
+                    case 2:
+                        WList.ShowList(A);
+                        break;
+                    case 3:
+                        WList.AddNewItem(A);
+                        break;
+                    case 4:
+                        WList.GetMax(A);
+                        break;
+                }
             }
-
         }
     }
 }
---
+++ WList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkList
+{
+    class WList
+    {
+        static void Error(string Text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{Text}");
+            Console.ResetColor();
+        }
+        public static void FillList(List<double> List)
+        {
+            Console.Clear();
+            Console.WriteLine("Введите длину массива");
+            int l = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Заполните массив");
+            for (int i = 0; i < l; i++)
+            {
+
+                List.Add(Convert.ToDouble(Console.ReadLine()));
+            }
+            Console.ReadKey();
+        }
+        public static void ShowList(List<double> List)
+        {
+            Console.Clear();
+            if (List.Count != 0)
+            {
+                for(int i = 0; i < List.Count; i++)
+                {
+                Console.WriteLine($"{i} элемент листа = {List[i]}");
+                }
+            }
+            else
+            {
+                Error("Вы еще не заполнили массив");
+            }
+            Console.ReadKey();
+        }
+        public static void AddNewItem(List<double> List)
+        {
+            Console.Clear();
+            int l;
+            Console.WriteLine("Сколько элементов хотите добавить?");
+            l = Convert.ToInt32(Console.ReadLine());
+            if(l > 0)
+            {
+                for(int i = 0; i < l; i++)
+                {
+                    List.Add(Convert.ToDouble(Console.ReadLine()));
+                }
+            }
+            else
+            {
+                Error("Вы ввели не верное кол-во элеметов");
+            }
+            Console.ReadKey();
+        }
+        public static void GetMax(List<double> List)
+        {
+            if(List.Count != 0)
+            {
+                Console.Clear();
+                Console.WriteLine($"Макс.элемент листа = {List.Max()}");
+            }
+            else
+            {
+                Error("Вы еще не заполнили массив");
+            }
+            Console.ReadKey();
+        }
+    }
+}
